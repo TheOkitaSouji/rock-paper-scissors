a3c8021bb67bbecc3ec170c98da5578d777ace9b
TheOkitaSouji <187431967+TheOkitaSouji@users.noreply.github.com>
2024-12-15 02:21:24 +0100

    Complete the code, game fully playable
    Completed everything necessary to make the game work

--- javascript.js
+++ javascript.js
@@ -10,55 +10,76 @@ function getComputerChoice() {
         computerChoice = "Scissors";
     }
     console.log(computerChoice);
+    return computerChoice;
 }
 
 let userInput = "";
 let humanChoice = "";
 function getHumanChoice() {
     userInput = prompt("Rock, Paper or Scissors?");
-    console.log(userInput);
-    if (userInput == "Rock") {
+    userInput = userInput.toLowerCase();
+    if (userInput == "rock") {
         humanChoice = "Rock";
-    } else if (userInput == "Paper") {
+    } else if (userInput == "paper") {
         humanChoice = "Paper";
-    } else if (userInput == "Scissors") {
+    } else if (userInput == "scissors") {
         humanChoice = "Scissors";
     } else {
         humanChoice = "Incorrect choice, choose either Rock, Paper or Scissors";
     }
     console.log(humanChoice);
+    return humanChoice;
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice = "Rock") && (computerChoice = "Scissors") {
+    if (humanChoice == computerChoice) {
+        console.log("It's a tie!");
+    } else if (humanChoice == "Rock" && computerChoice == "Scissors") {
         console.log("You win! Rock beats Scissors");
-    } else if (humanChoice = "Rock") && (computerChoice = "Paper") {
+        humanScore++
+    } else if (humanChoice == "Rock" && computerChoice == "Paper") {
         console.log("You lose! Paper beats Rock");
-    } else if (humanChoice = "Rock") && (computerChoice = "Rock") {
-        console.log("It's a tie!");
-    } else if (humanChoice = "Paper") && (computerChoice = "Rock") {
+        computerScore++
+    } else if (humanChoice == "Paper" && computerChoice == "Rock") {
         console.log("You win! Paper beats Rock");
-    } else if (humanChoice = "Paper") && (computerChoice = "Scissors") {
+        humanScore++
+    } else if (humanChoice == "Paper" && computerChoice == "Scissors") {
         console.log("You lose! Scissors beat Paper");
-    } else if (humanChoice = "Paper") && (computerChoice = "Paper") {
-        console.log("It's a tie!");
-    } else if (humanChoice = "Scissors") && (computerChoice = "Rock") {
+        computerScore++
+    } else if (humanChoice == "Scissors" && computerChoice == "Rock") {
         console.log("You lose! Rock beats Scissors");
-    } else if (humanChoice = "Scissors") && (computerChoice = "Paper") {
+        computerScore++
+    } else if (humanChoice == "Scissors" && computerChoice == "Paper") {
         console.log("You win! Scissors beat Paper");
-    } else if (humanChoice = "Scissors") && (computerChoice = "Scissors") {
-        console.log("It's a tie!");
+        humanScore++
     } else {
         console.log("Looks like you might've put in a wrong value, try again!")
     }
   }
-  
-  const humanSelection = getHumanChoice();
-  const computerSelection = getComputerChoice();
-  
-  playRound(humanSelection, computerSelection);
-  
-  //Check out what's wrong with if statements, try toLowerCase for prompt
+
+let rounds = 0;
+function playGame() {
+    rounds = 0;
+    while (rounds < 5) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+        rounds++
+        console.log("Round " + rounds + " finished");
+        console.log("Your score is " + humanScore);
+        console.log("Opponent's score is " + computerScore);
+    }
+    console.log("The final score is - Player: " + humanScore + " Computer: " + computerScore)
+    if (humanScore > computerScore) {
+        console.log("Congratulations, you win!")
+    } else if (humanScore < computerScore) {
+        console.log("Sadly you lose. Better luck next time!")
+    } else {
+        console.log("Looks like it's a tie!")
+    }
+}
+
+playGame();
